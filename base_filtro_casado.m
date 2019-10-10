6458d7763179533637f332e30e598f99110ff39e
Lenival Gomes <lenival@ufrn.edu.br>
2019-10-10 09:31:08 -0300

    Changed file codification to UFT8. Changed some object names. Added a estimation of BER (Bit Error Rate). Random message generation was adjusted.

--- base_filtro_casado.m
+++ base_filtro_casado.m
@@ -1,37 +1,35 @@
 
 %bits = [1 2 3 -3 -2 -1 2 -1 2 1 -2];         % Stream de bits
-dim = 4; bits = reshape(randi([-3,3],dim),[1,dim^2]);       % Stream de bits aleatÛrios
+%dim = 4; bits = reshape(randi([-3,3],dim),[1,dim^2]);       % Stream de bits aleat√≥rios
+l_m = 100;               % Comprimento da mensagem
+m_tx = (-1).^(round(rand(1,l_m))).*randi([1,3],1,l_m);       % Mensagem
 
-l_s = length(bits);              % Quantidade de bits enviados
-
-% Par‚metros do cosseno levantado
-sps = 4;                         % Quantidade de amostras/perÌodo
+% Par√¢metros do cosseno levantado
+sps = 4;                         % Quantidade de amostras/per√≠odo
 span = 6;                        % Quantidade de zeros
-beta = 0.5;                     % Fator de roll-off
+beta = 0.5;                      % Fator de roll-off
+T = 1+span*sps;                  % Largura do pulso em amostras
+N0 = 1.5;                          % Pot√™ncia do ru√≠do
 
-T = 1+span*sps;                        % Largura do pulso em amostras
-
-N0 = 1;
-
-up_bits = upsample(bits,T);
+up_bits = upsample(m_tx,T);      % Upsample da mensagem
 
 l_t = length(up_bits);
 
 t = 0:(l_t-1);
 
-% DefiniÁ„o do formatos de pulsos
-sw = 1*ones(1,T);      % Janela retangular
-% sw = rcosdesign(beta,span,sps);       % Cosseno levantado
+% Defini√ß√£o do formatos de pulsos
+sw = 1*ones(1,T);               % Janela retangular
+%sw = 2*rcosdesign(beta,span,sps);       % Cosseno levantado
 
 
-% CriaÁ„o do sinal transmitido
+% Cria√ß√£o do sinal transmitido
 sinal = conv(up_bits,sw);
 sinal = sinal(1:l_t);
 %sinal = sinal(T:end);
 %stem(t,sinal)
 
 
-% CriaÁ„o do ruÌdo de canal
+% Cria√ß√£o do ru√≠do de canal
 n = N0*randn(1,l_t);
 
 % Sinal recebido
@@ -44,17 +42,20 @@ mf = flip(sw);
 % Vetor de bits estimados
 Y_i = [];
 
-% Vetor de amplitudes mÈdias para comparaÁ„o
+% Mensagem detectada
+m_rx = [];
+
+% Vetor de amplitudes m√©dias para compara√ß√£o
 Y_mean = [];
 
-for k = 1:1:l_s
+for k = 1:1:l_m
     %[((k-1)*T+1) k*T]
     %r(((k-1)*T+1),k*T)
     Y_temp = 0;
     Y_temp = (1/T)*conv(r(((k-1)*T+1):k*T),mf);
     Y_i = [Y_i Y_temp(1:T)];
-    
-    % Calculando o valor mÈdio de cada bit para fazer a comparaÁ„o
+    m_rx = [m_rx  round(Y_temp(T))];
+    % Calculando o valor m√©dio de cada bit para fazer a compara√ß√£o
     % Y_mean = [Y_mean ones(1,T).*mean(r(((k-1)*T+1):k*T))];
     %Y = (1/T)*conv(r,mf);
 end
@@ -62,10 +63,24 @@ end
 %Y = (1/T)*conv(r,mf);
 %Y = Y(1:l_t);
 
+% Vetor de mensagens erradas
+error = (m_tx-m_rx)~=0;
+
+% Taxa de erro de bits
+ber = sum(error)/l_m
+
+figure;
 hold on
 stem(t,Y_i,'m')
 stem(t,sinal,'c')
 stem(t,r,'g')
-% stem(t,Y_mean,'r')
-% stem(t,Y,'r')
+%subplot(2,2,1)
+%stem(t,sinal,'c')
+%subplot(2,2,2)
+%stem(t,n,'c')
+%subplot(2,2,3)
+%stem(t,r,'g')
+%subplot(2,2,4)
+%stem(t,Y_i,'m')
+
 hold off
